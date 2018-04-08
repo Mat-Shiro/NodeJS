96372b7b6ee05982f4188148e317539aeffff1a5
Matheus Jacques <ml.jacques@hotmail.com>
2018-04-08 16:44:21 -0300

    Auto load do módulo dbConnection

--- curso_node/config/dbConnection.js
+++ curso_node/config/dbConnection.js
@@ -1,10 +1,16 @@
 var mysql = require('mysql');
 
-module.exports = function () {
+var connMySQL = function () {
+    console.log('Conexão com bd foi estabelecida');
     return mysql.createConnection({
         host: 'localhost',
         user: 'root',
         password: '1234',
         database: 'portal_noticias'
     });
+}
+
+module.exports = function () {
+    console.log('O autoload carregou o módulo de conexão com o bd');
+    return connMySQL;
 }
